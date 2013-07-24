758439bca437b6e8b384b3bcdb3d4b51b64e9da6
HardCorePawn <xxx@xxx>
2013-07-24 19:47:43 -0300

    Settings: Configure wake on Power, Home or Both [2/2] Allows users with Home button to specify which buttons to use to wake device. Options are:
    - Power Button only
    - Home Button only
    - Power or Home Button (default)
    
    Essentially the same as current functionality, but now enables user to
    "disable" Wake on Power Button, by selecting "Home Button only".
    
    Note: requires config_home_wakeScreen device overlay:
    
    packages/apps/Settings/res/values/bools.xml
    
    Based on Xposed addon by Daniel Bergløv aka SpazeDog
    https://github.com/SpazeDog/xposed-additions
    
    PS 2: change default to Both, refactor
    
    Change-Id: If45e537532d3f877c272e37253de3f70831b67bf
    
    Conflicts:
    	res/values/cm_arrays.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -66,7 +66,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_DISPLAY_ROTATION = "display_rotation";
     private static final String KEY_LOCKSCREEN_ROTATION = "lockscreen_rotation";
     private static final String KEY_WAKEUP_CATEGORY = "category_wakeup_options";
-    private static final String KEY_HOME_WAKE = "pref_home_wake";
+    private static final String KEY_BUTTON_WAKE = "pref_wakeon_button"; 
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
     private static final String KEY_SCREEN_OFF_ANIMATION = "screen_off_animation";
     private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label"; 
@@ -83,7 +83,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     private DisplayManager mDisplayManager;
 
-    private CheckBoxPreference mHomeWake;
+    private ListPreference mButtonWake;  
     private CheckBoxPreference mVolumeWake;
     private Preference mCustomLabel; 
     private CheckBoxPreference mDualPanel;
@@ -172,13 +172,17 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         PreferenceCategory wakeupCategory = (PreferenceCategory)
                     findPreference(KEY_WAKEUP_CATEGORY);
         // Home button wake
-        mHomeWake = (CheckBoxPreference) findPreference(KEY_HOME_WAKE);
-        if (mHomeWake != null) {
+        mButtonWake = (ListPreference) findPreference(KEY_BUTTON_WAKE);
+        if (mButtonWake != null) { 
             if (!res.getBoolean(R.bool.config_show_homeWake)) {
-                wakeupCategory.removePreference(mHomeWake);
+                //no home button, don't allow user to disable power button either
+                mWakeUpOptions.removePreference(mButtonWake); 
             } else {
-                mHomeWake.setChecked(Settings.System.getInt(resolver,
-                        Settings.System.HOME_WAKE_SCREEN, 1) == 1);
+                int buttonWakeValue = Settings.System.getInt(resolver,
+                        Settings.System.BUTTON_WAKE_SCREEN, 2);
+                mButtonWake.setValue(String.valueOf(buttonWakeValue));
+                mButtonWake.setSummary(getResources().getString(R.string.pref_wakeon_button_summary, mButtonWake.getEntry()));
+                mButtonWake.setOnPreferenceChangeListener(this); 
                 removeWakeupCategory = false;
             }
         }
@@ -459,11 +463,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mHomeWake) {
-            Settings.System.putInt(getContentResolver(), Settings.System.HOME_WAKE_SCREEN,
-                    mHomeWake.isChecked() ? 1 : 0);
-            return true;
-        } else if (preference == mVolumeWake) {
+        if (preference == mVolumeWake) {
             Settings.System.putInt(getContentResolver(), Settings.System.VOLUME_WAKE_SCREEN,
                     mVolumeWake.isChecked() ? 1 : 0);
             return true;
@@ -524,7 +524,14 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             } catch (NumberFormatException e) {
                 Log.e(TAG, "could not persist screen timeout setting", e);
             }
-        }
+        } else if (preference == mButtonWake) {
+            int buttonWakeValue = Integer.valueOf((String) objValue);
+            int index = mButtonWake.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.BUTTON_WAKE_SCREEN, buttonWakeValue);
+            mButtonWake.setSummary(getResources().getString(R.string.pref_wakeon_button_summary, mButtonWake.getEntries()[index]));
+            return true; 
+
         if (KEY_FONT_SIZE.equals(key)) {
             writeFontSizePreference(objValue);
         }
