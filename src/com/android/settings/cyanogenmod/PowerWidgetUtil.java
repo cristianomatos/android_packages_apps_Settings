4433eccf41d676f12907343f527aca3a0eebf178
cristianomatos <cristianobmatos@gmail.com>
2013-08-06 16:19:06 -0300

    Reboot button in Powerwidgets (2/2)
    Change-Id: I88571392a9c3baf0973d9b4628910b121eab81ac

--- src/com/android/settings/cyanogenmod/PowerWidgetUtil.java
+++ src/com/android/settings/cyanogenmod/PowerWidgetUtil.java
@@ -55,6 +55,7 @@ public class PowerWidgetUtil {
     public static final String BUTTON_MEDIA_NEXT = "toggleMediaNext";
     public static final String BUTTON_LTE = "toggleLte";
     public static final String BUTTON_WIMAX = "toggleWimax";
+    public static final String BUTTON_REBOOT = "toggleReboot"; 
 
     public static final HashMap<String, ButtonInfo> BUTTONS = new HashMap<String, ButtonInfo>();
     static {
@@ -120,6 +121,9 @@ public class PowerWidgetUtil {
         BUTTONS.put(BUTTON_WIMAX, new PowerWidgetUtil.ButtonInfo(
                 BUTTON_WIMAX, R.string.title_toggle_wimax,
                 "com.android.systemui:drawable/stat_wimax_on"));
+	BUTTONS.put(BUTTON_REBOOT, new PowerWidgetUtil.ButtonInfo(
+                BUTTON_REBOOT, R.string.title_toggle_reboot,
+                "com.android.systemui:drawable/stat_reboot"));
     }
 
     private static final String BUTTON_DELIMITER = "|";
