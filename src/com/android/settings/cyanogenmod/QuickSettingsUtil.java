d3ddae052bc0c0f25dcf45fe87a362a7a7e15258
cristianomatos <cristianobmatos@gmail.com>
2013-08-06 15:29:56 -0300

    Reboot tile to QS (1/2) - Still need to colapse expanded status bar and show reboot message
    Ported, with some adaptations, form Slim Roms
    
    Change-Id: Idc37622fa4dbf1edda68879d31f9019d2779e866

--- src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
+++ src/com/android/settings/cyanogenmod/QuickSettingsUtil.java
@@ -43,6 +43,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_USER;
 import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFI;
 import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
+import static com.android.internal.util.cm.QSConstants.TILE_REBOOT;   
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -145,6 +146,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_WIFIAP, R.string.title_tile_wifiap,
                 "com.android.systemui:drawable/ic_qs_wifi_ap_neutral"));
+	registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_REBOOT, R.string.title_tile_reboot,
+                "com.android.systemui:drawable/ic_qs_reboot"));  
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
