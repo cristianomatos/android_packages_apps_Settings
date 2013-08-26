e7fa2a67908bcba7bc6daf485359db98c6eb8422
Danny Baumann <dannybaumann@web.de>
2013-08-26 15:52:34 +0200

    Check for presence of hardware abstraction framework.
    Change-Id: I59da970d4a4eb74a571d1ac27cb18e351dfc2a53
    
    Conflicts:
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -184,7 +184,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 Settings.System.NOTIFICATION_SHOW_WIFI_SSID, 0) == 1); 
 
 	mAdaptiveBacklight = (CheckBoxPreference) findPreference(KEY_ADAPTIVE_BACKLIGHT);
-        if (!AdaptiveBacklight.isSupported()) {
+        if (!isAdaptiveBacklightSupported()) { 
             getPreferenceScreen().removePreference(mAdaptiveBacklight);
             mAdaptiveBacklight = null;
         } 
@@ -592,7 +592,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
      * @param ctx A valid context
      */
     public static void restore(Context ctx) {
-        if (AdaptiveBacklight.isSupported()) {
+        if (isAdaptiveBacklightSupported()) { 
             final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
             final boolean enabled = prefs.getBoolean(KEY_ADAPTIVE_BACKLIGHT, true);
             if (!AdaptiveBacklight.setEnabled(enabled)) {
@@ -600,6 +600,15 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             } else {
                 Log.d(TAG, "Adaptive backlight settings restored.");
             }
+        }
+    } 
+
+    private static boolean isAdaptiveBacklightSupported() {
+        try {
+            return AdaptiveBacklight.isSupported();
+        } catch (NoClassDefFoundError e) {
+            // Hardware abstraction framework not installed
+            return false;
         }
     } 
 }
